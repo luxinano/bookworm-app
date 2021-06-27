9238246c30fe07c392ac79fd29c4ab5c91b5f2c2
xuongbanh <xuong.banhtran@nashtechglobal.com>
2021-06-27 22:22:09 +0700

    update package.json: scss, react, bootstrap.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.sass('resources/css/app.scss', 'public/css');
+mix.js('resources/js/app.jsx', 'public/js').react();
